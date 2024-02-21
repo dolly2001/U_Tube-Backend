66760c64cace3a6587caf452ff4bee8e6825a079
dolly2001 <piyushsolanki18dec@gmail.com>
2024-02-22 02:20:20 +0530

    ApiResponse ErrorHandling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/dbconnection.js";
 import dotenv from "dotenv"
 
@@ -6,3 +7,20 @@ dotenv.config({
 })
 
 connectDB()
+
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log(error);
+        process.exit(1)
+    })
+
+    app.listen(process.env.PORT || 4000 , () => {
+        console.log(`YOUR SERVER IS LIVE NOW ON PORT ${process.env.PORT}`);
+    })
+
+})
+
+.catch((err) => {
+    console.log(`DATABASE CONNECTION ERROR : ${err}`);
+})
